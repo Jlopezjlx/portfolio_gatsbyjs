ffb08fe495380313a0d5bd166e0979de3e6c202f
Jairo <jlopez@oriontek.do>
2020-02-21 16:25:30 -0400

    aading projects page

--- src/components/landing/Projects/styles.js
+++ src/components/landing/Projects/styles.js
@@ -19,6 +19,30 @@ export const Grid = styled.div`
     grid-template-columns: 1fr;
   }
 `;
+export const Details = styled.div`
+  flex: 1;
+  padding-right: 2rem;
+
+  @media (max-width: 960px) {
+    padding-right: unset;
+    width: 100%;
+    order: 1;
+  }
+
+  h1 {
+    margin-bottom: 2rem;
+    font-size: 26pt;
+    color: #212121;
+  }
+
+  p {
+    margin-bottom: 2.5rem;
+    font-size: 20pt;
+    font-weight: normal;
+    line-height: 1.3;
+    color: #707070;
+  }
+`;
 
 export const Item = styled.div`
   width: 100%;
